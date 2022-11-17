6df89c0bbb09e83776bf7a166451ace0dca0ecb8
lejan22 <91455326+lejan22@users.noreply.github.com>
2022-11-17 23:00:22 +0100

    bruh

---
+++ Assets/Scripts/Exercise3.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise3 : MonoBehaviour
+{
+
+    private float jumpDistance = 60f;
+    private float limits=120f;
+    private Vector3 nextJump;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Like this we make the movement. If we press the button,and we didn't reach the limit, the player will move towards the indicated position
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y != limits)
+        {
+            nextJump = new Vector3(transform.position.x, transform.position.y + jumpDistance, transform.position.z);
+            transform.position = nextJump;
+            
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y != -limits)
+        {
+           nextJump= new Vector3(transform.position.x, transform.position.y - jumpDistance, transform.position.z);
+            transform.position = nextJump;
+            
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x != -limits)
+        {
+            nextJump = new Vector3(transform.position.x - jumpDistance, transform.position.y, transform.position.z);
+            transform.position = nextJump;
+            
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x != limits)
+        {
+            nextJump = new Vector3(transform.position.x + jumpDistance, transform.position.y, transform.position.z);
+            transform.position = nextJump;
+            
+        }
+    }
+
+}
---
+++ Assets/Scripts/Exercise4.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Exercise4 : MonoBehaviour
+{
+    public TextMeshProUGUI Option1text;
+    public TextMeshProUGUI Option2text;
+
+    public string[] Choice1;
+    public string[] Choice2;
+    
+
+    public int PCschoice; 
+
+   //Variable to know Which question to choose
+    private int question;
+
+    private int CorrectOption; 
+
+    void Start()
+    {
+       
+    }
+    //PC will gladly choose one of the two options
+    public void PcChoices() 
+    {
+        PCschoice = Random.Range(0, 2); 
+    }
+    //Lets go to the next question
+    public void Nextquestion() 
+    {
+        Option1text.text = Choice1[question];
+        Option2text.text = Choice2[question];
+    }
+
+    public void Choosing(int opt) 
+    {
+        if (opt == PCschoice) //if we have chosen the same option we add 1 point of success
+        {
+            CorrectOption++;
+        }
+
+        if (question < 9) //if the answer was not for the last question we can place the next two options on screen
+        {
+            question++;
+            PcChoices();
+            Nextquestion();
+        }
+
+       
+    }
+
+
+
+
+    
+}
+
